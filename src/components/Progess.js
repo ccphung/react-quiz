7501b3e8646373214644cec2bc540512ffcbc2ba
Cédric Phung <cedric3011@outlook.fr>
2025-02-19 13:43:23 +0100

    refactoring to context API

--- src/components/Progess.js
+++ src/components/Progess.js
@@ -1,4 +1,7 @@
-function Progess({ index, numQuestions, points, maxPossiblePoints, answer }) {
+import { useQuiz } from "../contexts/QuizContext";
+
+function Progess() {
+  const { index, numQuestions, points, maxPossiblePoints, answer } = useQuiz();
   return (
     <header className="progress">
       <progress max={numQuestions} value={index + Number(answer !== null)} />
